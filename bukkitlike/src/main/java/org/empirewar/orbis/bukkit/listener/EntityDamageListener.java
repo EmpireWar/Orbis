bdd3af7de79ef7a2bfbfbdafcab31d309cab6451
SamB440 <sam@islandearth.net>
2024-08-16 18:23:20 +0100

    spotlessApply

--- bukkitlike/src/main/java/org/empirewar/orbis/bukkit/listener/EntityDamageListener.java
+++ bukkitlike/src/main/java/org/empirewar/orbis/bukkit/listener/EntityDamageListener.java
@@ -1,3 +1,22 @@
+/*
+ * This file is part of Orbis, licensed under the GNU GPL v3 License.
+ *
+ * Copyright (C) 2024 EmpireWar
+ * Copyright (C) contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 package org.empirewar.orbis.bukkit.listener;
 
 import org.bukkit.Location;
@@ -22,7 +41,8 @@ public record EntityDamageListener(OrbisBukkit orbis) implements Listener {
         final Location location = player.getLocation();
         final Vector3d pos = new Vector3d(location.getX(), location.getY(), location.getZ());
 
-        final RegionQuery.Flag.Builder<Boolean> builder = RegionQuery.Flag.builder(DefaultFlags.INVULNERABILITY);
+        final RegionQuery.Flag.Builder<Boolean> builder =
+                RegionQuery.Flag.builder(DefaultFlags.INVULNERABILITY);
         builder.player(player.getUniqueId());
         final boolean canAct = world.query(RegionQuery.Position.builder().position(pos))
                 .query(builder)
