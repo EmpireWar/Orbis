f46774bfd3c47a7cf4f079c195681e1ffb1c7499
SamB440 <sam@islandearth.net>
2025-06-19 17:04:40 +0100

    Update license header.

--- paper/src/main/java/org/empirewar/orbis/paper/bootstrap/OrbisPluginBootstrap.java
+++ paper/src/main/java/org/empirewar/orbis/paper/bootstrap/OrbisPluginBootstrap.java
@@ -1,7 +1,7 @@
 /*
  * This file is part of Orbis, licensed under the GNU GPL v3 License.
  *
- * Copyright (C) 2025 EmpireWar
+ * Copyright (C) 2025 Empire War
  * Copyright (C) contributors
  *
  * This program is free software: you can redistribute it and/or modify
