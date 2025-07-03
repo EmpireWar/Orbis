f39789a147c5140d1786e411dccadeb5e0f6ac7e
SamB440 <sam@islandearth.net>
2025-07-03 15:46:41 +0100

    bukkitlike: Bump versions

--- bukkitlike/build.gradle.kts
+++ bukkitlike/build.gradle.kts
@@ -8,8 +8,8 @@ repositories {
 
 dependencies {
     compileOnly("org.slf4j:slf4j-api:2.0.12")
-    compileOnly("org.spigotmc:spigot-api:1.21.1-R0.1-SNAPSHOT")
-    compileOnly("net.kyori:adventure-api:4.17.0")
+    compileOnly("org.spigotmc:spigot-api:1.21.4-R0.1-SNAPSHOT")
+    compileOnly("net.kyori:adventure-api:4.21.0")
     implementation("org.incendo:cloud-paper:2.0.0-beta.10")
     implementation("org.incendo:cloud-annotations:2.0.0")
     implementation("org.incendo:cloud-minecraft-extras:2.0.0-beta.10")
